5193d90546aa67b153aa4ace7386603dd3f86dfe
Norihiro Kamae <norihiro@nagater.net>
2024-07-28 00:17:30 +0900

    util/rangeset.h: Add new API to test range is included
    This commit added a new API, which tests whether a part of the range
    [begin, end) has been set.
      bool test(const T &begin, const T &end) const;
    Also refactor the implementation of `add(const T begin, const T end)`
    and `test(const T &value)`.

--- util/rangeset.h
+++ util/rangeset.h
@@ -10,12 +10,14 @@ template<typename T> class rangeset {
 public:
 #endif
 	typedef std::pair<T, T> element_t;
-	typedef std::vector<element_t> container_t;
-	container_t set;
+	std::vector<element_t> set;
 
 public:
 	void add(const T begin, const T end)
 	{
+		/*  set:   [-- it0-1 --)      [-- it0 --)
+		 *  begin:                ^^^^^^^^^^^^^^^
+		 */
 		auto it0 = std::partition_point(set.begin(), set.end(),
 						[begin](const element_t &e) { return e.second < begin; });
 		if (it0 == set.end()) {
@@ -23,33 +25,50 @@ public:
 			return;
 		}
 
-		if (it0->first <= begin /* && begin <= it0->second */) {
-			it0->second = std::max(it0->second, end);
-		}
-		else if (/* begin < it0->first && */ it0->first <= end) {
-			it0->first = begin;
-			it0->second = std::max(it0->second, end);
-		}
-		else /* if (end < it0->first) */ {
+		if (end < it0->first) {
 			set.insert(it0, std::make_pair(begin, end));
 			return;
 		}
 
-		auto it1 = std::partition_point(it0, set.end(), [end](const element_t &e) { return e.first <= end; });
+		// Now, [begin, end) and it0 overlap. Merge into it0.
+		if (begin < it0->first)
+			it0->first = begin;
+		if (it0->second < end)
+			it0->second = end;
+		else
+			return;
 
-		typename container_t::iterator it1b = it1 - 1;
-		if (it1b->first <= end && end < it1b->second)
-			it0->second = std::max(it0->second, it1b->second);
+		auto it0n = it0 + 1;
+		if (it0n == set.end() || end < it0n->first)
+			return;
 
-		if (it0 != it1 && it0 + 1 != it1)
-			set.erase(it0 + 1, it1);
+		/*  set:   [-- it1-1 --)      [-- it1 --)
+		 *  end:               ^^^^^^^^^^^^^^^^
+		 */
+		auto it1 = std::partition_point(it0n, set.end(), [end](const element_t &e) { return e.second <= end; });
+		if (it1 != set.end() && it1->first <= end)
+			it0->second = it1++->second;
+		set.erase(it0n, it1);
 	}
 
 	bool test(const T &value) const
 	{
+		/*  set:   [-- it-1 --)      [-- it --)
+		 *  value:            ^^^^^^^^^^^^^^
+		 */
 		auto it = std::partition_point(set.begin(), set.end(),
-					       [value](const element_t &e) { return e.second < value; });
-		return it != set.end() && it->first <= value && value < it->second;
+					       [value](const element_t &e) { return e.second <= value; });
+		return it != set.end() && it->first <= value;
+	}
+
+	bool test(const T &begin, const T &end) const
+	{
+		/*  set:   [-- it-1 --)      [-- it --)
+		 *  begin:            ^^^^^^^^^^^^^^
+		 */
+		auto it = std::partition_point(set.begin(), set.end(),
+					       [begin](const element_t &e) { return e.second <= begin; });
+		return it != set.end() && it->first < end;
 	}
 };
 
