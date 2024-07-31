4b56578ff4860dff3ff5c63d7339206abf890d86
Norihiro Kamae <norihiro@nagater.net>
2024-07-31 21:10:57 +0900

    Fix building without libwebsockets

--- src/vmxinstance-list.h
+++ src/vmxinstance-list.h
@@ -4,5 +4,7 @@ ITEM(vmxhost_midi_type)
 #endif // WITH_PORTMIDI
 ITEM(vmxhost_dummy_type)
 ITEM(vmxserver_type)
+#ifdef WITH_WEBSOCKETS
 ITEM(vmxws_type)
+#endif // WITH_WEBSOCKETS
 ITEM(vmxmonitor_type)
